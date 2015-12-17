ee0ac2aa47429b2e901a2c431a5fbdafb6794ce2
Jeremy Thurgood <jeremy@praekelt.com>
2015-12-17 17:01:17 +0200

    Use consular from PPA instead of installing it in a venv.

--- puppet/modules/consular/manifests/init.pp
+++ puppet/modules/consular/manifests/init.pp
@@ -1,34 +1,15 @@
 class consular(
   $consular_args = [],
 ) {
-  class { 'python' :
-    version    => 'system',
-    pip        => 'present',
-    dev        => 'present',
-    virtualenv => 'present',
-    gunicorn   => 'absent',
-  }
-
-  package { [
-    'build-essential',
-    'libssl-dev',
-    'libffi-dev',
-  ]: }
+  apt::ppa { 'ppa:jerith/consular': ensure => 'present' }
   ->
-  file { '/var/consular':
-    ensure => directory,
-  }
-  ~>
-  file { '/var/consular/requirements.txt':
-    content => 'consular',
-  }
-  ~>
-  python::virtualenv { '/var/consular/venv':
-    requirements => '/var/consular/requirements.txt',
-  }
-  ~>
   file { '/etc/init/consular.conf':
     content => template('consular/init.consular.conf.erb'),
+  }
+  ~>
+  package { 'python-consular':
+    ensure => 'latest',
+    require => Class['apt::update'],
   }
   ~>
   service { 'consular':
