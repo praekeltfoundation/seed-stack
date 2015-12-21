5ec928ac26089afab5a4fea5ea5f065b10aa338b
Jamie Hewland <jhewland@gmail.com>
2015-12-21 09:01:43 +0200

    Make puppet scripts assume that we're in the puppet directory
    * Makes it easier to run things either on the guest or the host

--- Vagrantfile
+++ Vagrantfile
@@ -76,7 +76,9 @@ Vagrant.configure(2) do |config|
   # Enable provisioning with a shell script. Additional provisioners such as
   # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
   # documentation for more information about their specific syntax and use.
-  config.vm.provision :shell, path: "puppet/install-modules.sh"
+  config.vm.provision :shell do |shell|
+    shell.inline = "cd /vagrant/puppet && ./install-modules.sh"
+  end
   config.vm.provision :puppet do |puppet|
     puppet.module_path = ["puppet/upstream_modules", "puppet/modules"]
     puppet.manifests_path = "puppet/manifests"
--- puppet/run-tests.sh
+++ puppet/run-tests.sh
@@ -1,19 +1,18 @@
 #!/bin/bash -e
 
 # Run some basic tests for the Puppet modules and manifest.
-# NOTE: This script expects to be run from the repo root directory
 
 # Find and parse-check all the .pp files
-find puppet/modules -iname '*.pp' |\
-    xargs puppet parser validate --color false --render-as s --modulepath=puppet/modules
+find modules -iname '*.pp' |\
+    xargs puppet parser validate --color false --render-as s --modulepath=modules
 
-find puppet/upstream_modules -iname '*.pp' |\
-    xargs puppet parser validate --color false --render-as s --modulepath=puppet/upstream_modules
+find upstream_modules -iname '*.pp' |\
+    xargs puppet parser validate --color false --render-as s --modulepath=upstream_modules
 
 # Run the catalog test on the manifest
 puppet-catalog-test -v \
-    -m puppet/modules:puppet/upstream_modules \
-    -M puppet/manifests/default.pp \
+    -m modules:upstream_modules \
+    -M manifests/default.pp \
     --fact osfamily=Debian \
     --fact ipaddress=10.2.3.4 \
     --fact architecture=amd64 \
