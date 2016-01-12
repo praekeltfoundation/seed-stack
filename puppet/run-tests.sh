074251677c7cffe5202ca374e82b4ff68d607193
Jamie Hewland <jhewland@gmail.com>
2016-01-12 13:43:02 +0200

    Add ipaddress_eth1 fact in tests

--- puppet/run-tests.sh
+++ puppet/run-tests.sh
@@ -12,6 +12,7 @@ puppet-catalog-test -v \
     -M manifests/default.pp \
     --fact osfamily=Debian \
     --fact ipaddress_eth0=10.2.3.4 \
+    --fact ipaddress_eth1=192.168.0.2 \
     --fact architecture=amd64 \
     --fact operatingsystem=Ubuntu \
     --fact operatingsystemrelease=14.04 \
