77aca9775ae436d48290df4e126017d493705027
Jeremy Thurgood <jeremy@praekelt.com>
2016-04-26 15:22:09 +0200

    Kill commented code.

--- puppet/environments/seed_stack/manifests/site.pp
+++ puppet/environments/seed_stack/manifests/site.pp
@@ -46,19 +46,10 @@ node 'standalone.seed-stack.local' {
 # Keep track of node IP addresses across the cluster
 # FIXME: A better, more automatic way to do this
 class seed_stack_cluster {
+  # The hostmanager vagrant plugin manages the hosts entries for us, but
+  # various things still need the IPs. Bleh.
   $controller_ip = '192.168.55.11'
   $worker_ip = '192.168.55.21'
-
-  # The hostmanager vagrant plugin manages the hosts entries for us.
-
-  # host { 'controller.seed-stack.local':
-  #   ip           => $controller_ip,
-  #   host_aliases => ['controller'],
-  # }
-  # host { 'worker.seed-stack.local':
-  #   ip           => $worker_ip,
-  #   host_aliases => ['worker'],
-  # }
 }
 
 node 'controller.seed-stack.local' {
