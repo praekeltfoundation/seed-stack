197ae7c429853c19ef119e9fba7377c2a1c98a84
Nelson Koma <koma.nelson@gmail.com>
2016-05-25 17:24:56 +0200

    Fix trailing spaces for real now

--- puppet/environments/seed_stack/manifests/site.pp
+++ puppet/environments/seed_stack/manifests/site.pp
@@ -34,14 +34,12 @@ $sedcmd = 's@/usr/bin/curl@/opt/mesosphere/bin/curl@'
 file { '/tmp/dcos':
     ensure => 'directory',
   }
-
 exec { 'get-dcos-installer':                    
   command => 'curl -O http://boot.seed-stack.local:9012/dcos_install.sh',
   cwd     => 'tmp/dcos/',
   creates => '/tmp/dcos/dcos_install.sh',
   require => File['/tmp/dcos'],
   }
-
 exec { 'run-dcos-installer':                    
   command => "sed -i 's/devicemapper/deceivemapper/' dcos_install.sh \
               bash dcos_install.sh ${dcos_role} \
